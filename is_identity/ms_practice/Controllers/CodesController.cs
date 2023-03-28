587cd6221e111b3cfb73fdd4bf49d6b202166310
Misari112 <84647036+Misari112@users.noreply.github.com>
2023-03-27 19:27:55 -0500

    add basic compiler

---
+++ is_identity/ms_practice/Controllers/CodesController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ms_practice.Models;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ms_practice.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CodesController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public CodesController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        // GET: api/<CodesController>
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            _httpClient.BaseAddress = new Uri("https://api.jdoodle.com/");
+            var data = new { 
+                clientId = "90ea5f4576609a4f4ec9dee087e9ec80",
+                clientSecret = "6cdc7290f0e1b67d29e467274b5c7a8cc030651d03188b13c196cd531d7397d8",
+                script = "print(\"Hola, mundo!\")\r\n",
+                stdin = "",
+                language = "python2",
+                versionIndex = "0",
+                compileOnly = false,
+            };
+
+            var content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("v1/execute", content);
+
+            string contenido = await response.Content.ReadAsStringAsync();
+
+            return contenido;
+        }
+
+        // GET api/<CodesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CodesController>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CodeRequest request)
+        {
+            _httpClient.BaseAddress = new Uri("https://api.jdoodle.com/");
+            var data = new
+            {
+                clientId = "90ea5f4576609a4f4ec9dee087e9ec80",
+                clientSecret = "6cdc7290f0e1b67d29e467274b5c7a8cc030651d03188b13c196cd531d7397d8",
+                script = request.Script,
+                stdin = request.Stdin,
+                language = "python2",
+                versionIndex = "0",
+                compileOnly = false,
+            };
+
+            var content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("v1/execute", content);
+
+            string contenido = await response.Content.ReadAsStringAsync();
+
+            return new OkObjectResult(contenido);
+        }
+
+        // PUT api/<CodesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CodesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ is_identity/ms_practice/Models/CodeRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ms_practice.Models
+{
+    public class CodeRequest
+    {
+        public string Script { get; set; }
+        public string Stdin { get; set; }
+    }
+}
